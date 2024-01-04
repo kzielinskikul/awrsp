8f0891a26a6aeded6989c06d1220574b40a6ceb9
Kamil Zieliński <kp.zielinski@gmail.com>
2024-01-04 18:15:45 +0100

    Changes in Second.java

--- src/Second.java
+++ src/Second.java
@@ -1,2 +1,25 @@
 public class Second {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void main2(String[] args) {
+        for (int i = 0; i < 10; i++) {
+
+        }
+
+        for (int i = 0; i < 10; i++) {
+
+        }
+
+        for (int i = 0; i < 11; i++) {
+
+        }
+    }
+
+    public static void main3(String[] args) {
+
+    }
+
 }
